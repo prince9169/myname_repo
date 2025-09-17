cfad663cd42f47acf73367d8653fc70b38cd4ac8
Ubuntu <ubuntu@ip-172-31-7-126.ap-south-1.compute.internal>
2025-09-17 14:49:16 +0000

    first commit

---
+++ cal.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo "Print first number : "
+read a
+
+echo "Print second number : "
+read b
+
+x=$((a + b))
+
+echo "$x"
+
+#echo "Enter first number: "
+#read num1
+#echo "Enter second number: "
+#read num2
+
+# Perform addition
+#sum=$((num1 + num2))
+
+#echo "The sum of $num1 and $num2 is: $sum"
